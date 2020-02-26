cdb8fac939d318d0d2eb603c3fef733335c031d1
Lester Young <lyoung070789@gmail.com>
2020-02-26 18:37:33 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,10 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+
+p doc.css("#3c239880-48cf-5ce7-84d0-578546d87586 .inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")[0].name
+
+# courses.each do |course|
+#   puts course.text
+# end 
